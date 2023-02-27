12693c2dccaa94ca133ed1c306070804b9fffaf7
Andrew Conlin <andrewpc1013@gmail.com>
2023-02-27 14:36:38 -0600

    set up basic project structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Navbar } from "./";
+import { Navbar, Home } from "./";
+import { Routes, Route } from "react-router-dom";
 
 const Main = () => {
     return(
         <div id="main">
             <Navbar />
+            <div id="container">
+                <Routes>
+                    <Route exact path="/" element = {
+                        <Home/>
+                    }/>
+                </Routes>
+            </div>
         </div>
     )
 }
